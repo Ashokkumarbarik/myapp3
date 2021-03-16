09e9eb8e9548780bb8092e95bbf8aaa2a3021d86
Ashokkumarbarik <ashokhope66@gmail.com>
2021-03-16 12:59:47 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myapp3';
+export class AppComponent implements OnInit {
+  title = 'angularAssignement';
+  tasks: Task[] = [];
+  allTasks: Task[] = [];
+  activeTasks: Task[] = [];
+  completedTasks: Task[] = [];
+  activeItems: any;
+
+  ngOnInit() {
+    this.assignedValue();
+    this.allTasks = this.tasks;
+    this.getActiveItems();
+
+  }
+
+
+  onAll() {
+    this.allTasks = this.tasks;
+    this.getActiveItems();
+
+  }
+  onActive() {
+    this.allTasks = this.tasks.filter(x => !x.isComplete);
+    this.getActiveItems();
+  }
+  onCompleted() {
+    this.allTasks = this.tasks.filter(x => x.isComplete);
+
+   this.getActiveItems();
+  }
+  onClear() {
+    this.allTasks = [];
+    this.activeItems = null;
+  }
+
+  private assignedValue() {
+    let task = new Task();
+    task.isComplete = false;
+    task.text = 'One';
+    this.tasks.push(task);
+    let task1 = new Task();
+    task1.isComplete = true;
+    task1.text = 'Two';
+    this.tasks.push(task1);
+    let task3 = new Task();
+    task3.isComplete = true;
+    task3.text = 'Three';
+    this.tasks.push(task3);
+  }
+  private getActiveItems() {
+    this.activeItems = this.tasks.filter(x => !x.isComplete).length;
+  }
+
 }
+
+
---
+++ src/app/app.model.ts
@@ -0,0 +1,4 @@
+export class Task {
+  isComplete!: boolean;
+  text!: string;
+}
